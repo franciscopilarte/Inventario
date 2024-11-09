f7939c57117c051f5563e6012336258a2d7f83b5
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-11-08 21:18:57 -0600

    se creo la clase inventario, con una lista para guardar los producto y metodo de filtracion y ordenado usando LINQ

---
+++ Inventario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario
+{
+    public class Inventario
+    {
+        private List<Producto> productos;
+
+        public Inventario()
+        {
+            productos = new List<Producto>(); // Inicializamos la lista
+        }
+
+        public void AgregarProducto(Producto producto)
+        {
+            productos.Add(producto);
+        }
+
+        public IEnumerable<Producto> FiltrarYOrdenarProducto(decimal precioMinimo)
+        {
+            return productos
+                .Where(p => p.Precio > precioMinimo)
+                .OrderBy(p => p.Precio);
+        }
+
+
+
+
+    }
+}
