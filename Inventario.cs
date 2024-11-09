fdc3adcdd7d1c140034c05b3ebd53b3c50d381e6
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-11-08 22:02:18 -0600

    se creo un objeto de inventario y de producto y for para la lista de producto para que le usario ingrese la cantidad de producto sus nombre y precio

--- Inventario.cs
+++ Inventario.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Inventario
+namespace Tarea
 {
     public class Inventario
     {
--- Program.cs
+++ Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using System;
+
+namespace Tarea
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Inventario inventario = new Inventario();
+            Console.WriteLine("\nBIENVENIDO AL SISTEMA DE GESTION DE INVENTARIO");
+            Console.WriteLine();
+
+            int cantidad;
+            do
+            {
+                Console.WriteLine("¿Cuántos productos desea ingresar?");
+                if (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad < 0)
+                {
+                    Console.WriteLine("Ingrese un número positivo.");
+                }
+            } while (cantidad < 0);
+
+            // Ciclo para ingresar productos
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine($"\nProducto {i + 1}");
+                Console.WriteLine("Nombre:");
+                string nombre = Console.ReadLine();
+
+                decimal precio;
+                do
+                {
+                    Console.WriteLine("Precio:");
+                    if (!decimal.TryParse(Console.ReadLine(), out precio) || precio < 0)
+                    {
+                        Console.WriteLine("Ingrese un número positivo.");
+                    }
+                } while (precio < 0);
+
+                Producto producto = new Producto(nombre, precio);
+                inventario.AgregarProducto(producto);
+            }
+
+
+        }
+
+    }
+}
