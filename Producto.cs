29118e6e9ab36b749bd08093ee1ca9814edaca26
Francisco Pilarte <fpilarteespinosa@gmail.com>
2024-11-08 21:11:31 -0600

    se creo la clase producto con dos atributo, un metodo constructor y un metodo para mostrar la informacion

---
+++ Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario
+{
+    public class Producto
+    {
+        public Producto(string nombre, decimal precio)
+        {
+            Nombre = nombre;
+            Precio = precio;
+        }
+
+        public string Nombre { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Producto: {Nombre}, Precio: {Precio}");
+        }
+
+
+
+    }
+}
